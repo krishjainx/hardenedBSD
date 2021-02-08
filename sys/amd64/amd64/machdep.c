5efd7267e940a43185227e6a33f4eb12eb0112bd
Shawn Webb <shawn.webb@hardenedbsd.org>
2021-02-08 14:50:06 -0500

    HBSD: Harden amd64 machdep.efi_map
    The sysctl node `machdep.efi_map` is another "infoleak as feature" we
    inherited from FreeBSD. Harden this node by flagging it with
    CTLFLAG_ROOTONLY. Only privileged, non-jailed users can now access the
    node.
    
    This node is especially sensitive due to showing mappings with both
    physical and virtual addresses.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    issue:		#28
    MFC-to:		13-STABLE
    MFC-to:		12-STABLE
    (cherry picked from commit 4f15d79c4b7b0396b92965cb0eac4c3494cf0e73)
    Signed-off-by: Shawn Webb <shawn.webb@hardenedbsd.org>

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -1981,8 +1981,9 @@ efi_map_sysctl_handler(SYSCTL_HANDLER_ARGS)
 	efisize = *((uint32_t *)efihdr - 1);
 	return (SYSCTL_OUT(req, efihdr, efisize));
 }
-SYSCTL_PROC(_machdep, OID_AUTO, efi_map, CTLTYPE_OPAQUE|CTLFLAG_RD, NULL, 0,
-    efi_map_sysctl_handler, "S,efi_map_header", "Raw EFI Memory Map");
+SYSCTL_PROC(_machdep, OID_AUTO, efi_map, CTLTYPE_OPAQUE|CTLFLAG_RD
+    | CTLFLAG_ROOTONLY, NULL, 0, efi_map_sysctl_handler,
+    "S,efi_map_header", "Raw EFI Memory Map");
 
 void
 spinlock_enter(void)
