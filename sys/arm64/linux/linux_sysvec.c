cfa110468931373866ed491d7ac9350d0ad6f4eb
Shawn Webb <shawn.webb@hardenedbsd.org>
2022-02-18 19:44:42 -0500

    HBSD: Resolve merge conflicts
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- sys/arm64/linux/linux_sysvec.c
+++ sys/arm64/linux/linux_sysvec.c
@@ -174,16 +174,9 @@ linux_copyout_auxargs(struct image_params *imgp, uintptr_t base)
 
 	issetugid = p->p_flag & P_SUGID ? 1 : 0;
 	AUXARGS_ENTRY(pos, LINUX_AT_SYSINFO_EHDR,
-<<<<<<< HEAD
 	    imgp->proc->p_shared_page_base);
-#if 0	/* LINUXTODO: implement arm64 LINUX_AT_HWCAP */
-	AUXARGS_ENTRY(pos, LINUX_AT_HWCAP, cpu_feature);
-#endif
-=======
-	    imgp->proc->p_sysent->sv_shared_page_base);
 	AUXARGS_ENTRY(pos, LINUX_AT_HWCAP, *imgp->sysent->sv_hwcap);
 	AUXARGS_ENTRY(pos, AT_PAGESZ, args->pagesz);
->>>>>>> origin/freebsd/13-stable/main
 	AUXARGS_ENTRY(pos, LINUX_AT_CLKTCK, stclohz);
 	AUXARGS_ENTRY(pos, AT_PHDR, args->phdr);
 	AUXARGS_ENTRY(pos, AT_PHENT, args->phent);
@@ -436,14 +429,10 @@ struct sysentvec elf_linux_sysvec = {
 	.sv_shared_page_len = PAGE_SIZE,
 	.sv_schedtail	= linux_schedtail,
 	.sv_thread_detach = linux_thread_detach,
-<<<<<<< HEAD
-	.sv_trap	= linux_vsyscall,
 	.sv_pax_aslr_init = pax_aslr_init_vmspace,
-=======
 	.sv_trap	= NULL,
 	.sv_hwcap	= &elf_hwcap,
 	.sv_hwcap2	= &elf_hwcap2,
->>>>>>> origin/freebsd/13-stable/main
 	.sv_onexec	= linux_on_exec,
 	.sv_onexit	= linux_on_exit,
 	.sv_ontdexit	= linux_thread_dtor,
